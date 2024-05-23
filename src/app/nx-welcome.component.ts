9cbecbf72ad4b2783169fe97d97fb3c8d4115fa9
Lara Newsom <larnewso@cisco.com>
2024-05-23 16:33:56 -0500

    minor change to test pipeline

--- src/app/nx-welcome.component.ts
+++ src/app/nx-welcome.component.ts
@@ -6,13 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <!--
-     * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-     This is a starter component and can be deleted.
-     * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-     Delete this file and get started with your project!
-     * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-     -->
     <style>
       html {
         -webkit-text-size-adjust: 100%;
