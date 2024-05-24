fe4b5db856523cffba8110a1752cd3554f58c83d
Lara Newsom <larnewso@cisco.com>
2024-05-24 13:27:40 -0500

    added rabbit route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'rabbits',
+        loadComponent: () =>
+            import('@lara-test/rabbits').then(
+                (m) => m.RabbitsComponent,
+            )
+    }
+];
