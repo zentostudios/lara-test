63011a9283f39a73f5bafaabf581dd29d2cf56b5
Lara Newsom <larnewso@cisco.com>
2024-05-28 15:37:44 -0500

    rabbit baby component

---
+++ libs/rabbits/src/lib/rabbits/rabbit-baby.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-rabbit-baby',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<div>rabbit baby!!</div>`,
+})
+export class RabbitBabyComponent {}
--- libs/rabbits/src/lib/rabbits/rabbits.component.ts
+++ libs/rabbits/src/lib/rabbits/rabbits.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RabbitBabyComponent } from './rabbit-baby.component';
 
 @Component({
   selector: 'lib-rabbits',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RabbitBabyComponent,
+  ],
   templateUrl: './rabbits.component.html',
   styleUrl: './rabbits.component.css',
 })
